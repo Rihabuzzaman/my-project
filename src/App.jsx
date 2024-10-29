abf4dce95d1c61362abd205946293d128b665c8a
Rihabuzzaman <rehabhossen53@gmail.com>
2024-10-30 00:03:44 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
+import './App.css';
+import Navbar from './components/Daisyui/Navbar/Navbar';
+// import DsisyNav from './components/Daisyui/DsisyNav';
 
 function App() {
-
   return (
     <>
-    
-      <h1 className='text-4xl bg-red-500'>Vite + React</h1>
-      
-     
+    <Navbar></Navbar>
+      {/* <DsisyNav></DsisyNav> */}
+      <h1 className="text-4xl bg-red-500">Vite + React</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
